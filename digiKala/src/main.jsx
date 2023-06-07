81f518adfe18ffebb015b31a2f6155d50b0bad05
masoud <masoud.red64@gamil.com>
2023-06-07 14:32:03 +0330

    create structure of project

--- digiKala/src/main.jsx
+++ digiKala/src/main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "./assets/styles/reset.css";
+import "./assets/styles/default.css";
+import "./assets/styles/font.css";
+import "./assets/styles/grid.css";
+import "./assets/styles/custom.css";
 import App from "./App.jsx";
 import "./index.css";
+import "./assets/styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
