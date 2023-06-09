c55da147b595dd94194f4db875c98149b566a150
masoud <masoud.red64@gamil.com>
2023-06-09 12:45:43 +0330

    swiper js in index.js

---
+++ digiKala/src/Components/Swiper/SwiperJs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Swiper.css";
+
+export default function SwiperJs() {
+  return (
+    <>
+      <Swiper
+        navigation={true}
+        pagination={true}
+        modules={[Navigation, Pagination, Autoplay]}
+        loop={true}
+        autoplay={{ delay: 4000 }}
+        className="mySwiper top-swiper__swiper"
+      >
+        <SwiperSlide>
+          <img src="./images/slide1.webp" alt="" className="top-swiper__img" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./images/slide2.webp" alt="" className="top-swiper__img" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./images/slide2.webp" alt="" className="top-swiper__img" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- digiKala/src/Pages/Index/Index.jsx
+++ digiKala/src/Pages/Index/Index.jsx
@@ -3,11 +3,13 @@ import Header from "../../Components/Header/Header";
 
 import "./Index.css";
 import Footer from "../../Components/Footer/Footer";
+import SwiperJs from "../../Components/Swiper/SwiperJs";
 
 export default function Index() {
   return (
     <>
       <Header />
+      <SwiperJs />
       <Footer />
     </>
   );
