317361a22e98bee68f1248c024fedd11c13f1da7
masoud <masoud.red64@gamil.com>
2023-06-10 13:49:12 +0330

    selected products section

--- digiKala/src/Pages/Index/Index.jsx
+++ digiKala/src/Pages/Index/Index.jsx
@@ -14,6 +14,7 @@ import BaseVisitedCategories from "../../Components/BaseVisitedCategories/BaseVi
 import DigiPlus from "../../Components/DigiPlus/DigiPlus";
 import DigiClub from "../../Components/DigiClub/DigiClub";
 import BestSelling from "../../Components/BestSelling/BestSelling";
+import SelectedProducts from "../../Components/SelectedProducts/SelectedProducts";
 
 export default function Index() {
   return (
@@ -128,6 +129,7 @@ export default function Index() {
           <DigiClub />
           <BaseVisitedCategories />
           <BestSelling />
+          <SelectedProducts />
         </div>
       </div>
       <Footer />
