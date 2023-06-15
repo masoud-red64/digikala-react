076dd1dbac95e92bb104872e5a852b9457bbe826
masoud <masoud.red64@gamil.com>
2023-06-15 15:41:57 +0330

    cart handle navbar for change cart and nextCart

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,42 +1,63 @@
 import React, { useState } from "react";
 import "./Cart.css";
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 import NotEmptyCart from "../../Components/CartPage/NotEmptyCart/NotEmptyCart";
 import EmptyCart from "../../Components/CartPage/EmptyCart/EmptyCart";
 import EmptyNextCart from "../../Components/CartPage/EmptyNextCart/EmptyNextCart";
 import NotEmptyNextCart from "../../Components/CartPage/NotEmptyNextCart/NotEmptyNextCart";
 
 export default function Cart() {
+  const [isShowNotEmptyCart, setIsShowNotEmptyCart] = useState(false);
+  const [isShowNotEmptyNextCart, setIsShowNotEmptyNextCart] = useState(false);
+  const [isShowEmptyCart, setIsShowEmptyCart] = useState(true);
+  const [isShowEmptyNextCart, setIsShowEmptyNextCart] = useState(false);
+  const [navbarTitle, setNavbarTitle] = useState("cart");
+
   return (
-   <>
-   <Header/>
-   
-   <div className="main">
-      <div className="container">
-        <div className="cart-navbar">
-          <ul className="cart-navbar__list">
-            <li
-              className="cart-navbar__item cart-navbar__item--active"
-              data-nav-cart="cart"
-            >
-              سبد خرید
-              <span className="cart-navbar__item-cart-count">۱</span>
-            </li>
-            <li className="cart-navbar__item" data-nav-cart="next-cart">
-              خرید بعدی
-              <span className="cart-navbar__item-next-cart-count">۱</span>
-            </li>
-          </ul>
+    <>
+      <Header />
+
+      <div className="main">
+        <div className="container">
+          <div className="cart-navbar">
+            <ul className="cart-navbar__list">
+              <li
+                className={`cart-navbar__item ${
+                  navbarTitle === "cart" ? "cart-navbar__item--active" : ""
+                }`}
+                onClick={() => {
+                  setNavbarTitle("cart");
+                  setIsShowEmptyCart(true);
+                  setIsShowEmptyNextCart(false);
+                }}
+              >
+                سبد خرید
+                <span className="cart-navbar__item-cart-count">۱</span>
+              </li>
+              <li
+                className={`cart-navbar__item ${
+                  navbarTitle === "next-cart" ? "cart-navbar__item--active" : ""
+                }`}
+                onClick={() => {
+                  setNavbarTitle("next-cart");
+                  setIsShowEmptyCart(false);
+                  setIsShowEmptyNextCart(true);
+                }}
+              >
+                خرید بعدی
+                <span className="cart-navbar__item-next-cart-count">۱</span>
+              </li>
+            </ul>
+          </div>
+          {isShowNotEmptyCart && <NotEmptyCart />}
+          {isShowEmptyCart && <EmptyCart />}
+          {isShowNotEmptyNextCart && <NotEmptyNextCart />}
+          {isShowEmptyNextCart && <EmptyNextCart />}
         </div>
-        <EmptyCart />
-        <NotEmptyCart />
-        <EmptyNextCart />
-        <NotEmptyNextCart />
       </div>
-    </div>
-   
-   <Footer/>
-   </>
+
+      <Footer />
+    </>
   );
 }
